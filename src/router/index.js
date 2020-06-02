0e178de5483f3e522b11ec69095c1866b24bcf2d
leandroblanco456 <leandro.blanco@alumnos.frm.utn.edu.ar>
2020-06-02 15:08:47 -0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,21 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/productos',
+    name: 'Productos',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Productos.vue')
+  },
+  {
+    path: "/detalle/:id",
+    name: "DetalleInstrumento",
+    component: () => import("../views/DetalleInstrumento.vue")
   }
+
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <h1>Musical Hendrix es una tienda de instrumentos musicales con ya más de 15 años de experiencia. Tenemos el
+conocimiento y la capacidad como para informarte acerca de las mejores elecciones para tu compra musical.</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
